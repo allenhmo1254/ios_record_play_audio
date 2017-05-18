4c1cc19862b8bd0db13089d542f042c4ffb61618
景中杰 <jingzhongjie@jingzhongjiedeMacBook-Pro.local>
2017-05-18 19:18:23 +0800

    git提交测试

--- RecordAudio/RecordAudio/RootViewController.m
+++ RecordAudio/RecordAudio/RootViewController.m
@@ -23,6 +23,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    NSLog(@"补丁测试");
 }
 
 - (void)didReceiveMemoryWarning {
